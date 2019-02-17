38f1bc380f81ee679760b30192e88200418c5b3f
Kenneth Nuñezca <manilaboy07@gmail.com>
2019-02-17 21:44:05 +0800

    Update script.js

--- src/script.js
+++ src/script.js
@@ -90,7 +90,7 @@ document.getElementById('scrollHomeDown').addEventListener("click", function() {
 // ====================== BUTTON TO TOP ====================== //
 
 document.getElementById('btnTop').addEventListener("click", function() {
-	location = "#skrollr-body";
+	location = "#home1";
 });
 
 
